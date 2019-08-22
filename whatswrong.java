a76359bdb4c54ec826851d7169457337e04bb858
Carolyn Chen <cgc731@gmail.com>
2019-08-22 15:54:49 -0400

    Update whatswrong.java

--- whatswrong.java
+++ whatswrong.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+class Main {
+  public static void main(String[] args) {
+    
+    Scanner in = new Scanner(System.in);
+    
+    System.out.println("Hi! How are you feeling today? 
+                       A. Happy
+                       B. Sad
+                       C. Unmotivated/Uninspired
+                       D. Moody
+                       E. Homesick
+                       F. Anxious
+                       G. Self-critical
+                       H. Apathetic
+                       Select one of the above by typing the corresponding letter on your keyboard." ) ; 
+     
+     String user_input = in.NextLine(); 
+                       
+  }
