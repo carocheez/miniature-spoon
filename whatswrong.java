43bba90f3cfa307fb61e8d69ffaa6e7580ccfa06
Carolyn Chen <cgc731@gmail.com>
2019-08-22 16:21:06 -0400

    Update whatswrong.java

--- whatswrong.java
+++ whatswrong.java
@@ -6,22 +6,25 @@ class Main {
     
     Scanner in = new Scanner(System.in);
     
-    System.out.println("Hi there! How are you feeling today? 
-                       A. Happy
-                       B. Sad
-                       C. Unmotivated/Uninspired
-                       D. Moody
-                       E. Homesick
-                       F. Anxious
-                       G. Self-critical
-                       H. Apathetic
+    System.out.println("Hi there! What's wrong? How are you feeling today? 
+
+                       A. Unmotivated/Uninspired
+                       B. Homesick
+                       C. Anxious
+                       D. Self-critical
+                       
                        Select one of the above by typing the corresponding letter on your keyboard." ) ; 
                        
      //accepting user input
      String user_input = in.NextLine(); 
      
+     // list of my responses based on user_input, which will be randomized to make things fun!
+     
+     String [] a = {"So you're feeling unmotivated. That's okay; everyone has those days. Keep your head up; great things are coming. ", 
+                    "So you're feeling unmotivated. Write down a list of items to complete, start with the small things, and hopefully you will feel more motivated to do bigger tasks after this!"};
                        
-                       
+     String [] b = {"So you're feeling sad. Identify what's causing you to be in a bad mood. Keep your head up; great things are coming." ,
+                    "So you're feeling sad. 
                        
      // what to print based on the user_input
                        
@@ -41,19 +44,6 @@ class Main {
        System.out.println(" ")
      }   
                        
-     if user_input == ("E") {
-       System.out.println(" ")
-     }                   
-      
-     if user_input == ("F") {
-       System.out.println(" ")
-     }   
-                       
-     if user_input == ("G") {
-       System.out.println(" ")
-     }
-                       
-     if user_input == ("H") {
-       System.out.println(" ")
-     }                  
+     
+                  
   }
