6d0a1910cc9ad785aa25bd61129a5584839c6952
Carolyn Chen <cgc731@gmail.com>
2019-08-22 16:28:10 -0400

    Update whatswrong.java

--- whatswrong.java
+++ whatswrong.java
@@ -23,8 +23,14 @@ class Main {
      String [] a = {"So you're feeling unmotivated. That's okay; everyone has those days. Keep your head up; great things are coming. ", 
                     "So you're feeling unmotivated. Write down a list of items to complete, start with the small things, and hopefully you will feel more motivated to do bigger tasks after this!"};
                        
-     String [] b = {"So you're feeling sad. Identify what's causing you to be in a bad mood. Keep your head up; great things are coming." ,
-                    "So you're feeling sad. 
+     String [] b = {"So you're feeling homesick. It's totally normal to feel homesick after moving away from your entire life and starting a new one. Call home to hear from people who know and love you!" ,
+                    "So you're feeling homesick. get over it. lol jk" };
+                       
+     String [] c = {"So you're feeling anxious." , 
+                    "So you're feeling anxious." };
+                       
+     String [] d = {"So you're feeling self-critical." , 
+                    "So you're feeling self-critical." };                  
                        
      // what to print based on the user_input
                        
